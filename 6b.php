e1e70cb40e92cddf11a23f8c9485c506d2f83a7a
Maarten Vliegenthart <maarten@maximum.nl>
2024-11-06 20:53:00 +0100

    6b

--- 6b.php
+++ 6b.php
@@ -1,41 +1,29 @@
 <?php
-
+$startTime = microtime(true);
 $input = file('input6.txt');
 //echo $input[0] . "\n";
 $fishes = explode( ',', $input[0]);
 
-//$fishes = [8, 8];
-
 $total = 0;
 $totalDays = 256;
 $cache = [];
-$globalFishCounter = 0;
 foreach ($fishes as $fish) {
-    $globalFishCounter++;
     $currentFishes = [$fish];
     $currentFishTotal = 0;
     $numberOfRemovedFishes = 0;
-    //echo $globalFishCounter . PHP_EOL;
     for ($day = 1; $day <= $totalDays; $day++) {
-        //echo PHP_EOL;
         $fishesCount = count($currentFishes);
+        $daysToGo = $totalDays - $day;
         if ($fishesCount === 0) {
-            //echo 'finished for this fish, continue to next one' . PHP_EOL;
+            if (!isset($cache[$fish][$day]) && $daysToGo === 0) {
+                $cache[$fish][$day] = $currentFishTotal + count($currentFishes);
+            }
             continue;
         }
-        $daysToGo = $totalDays - $day;
-        //echo 'fish count: ' . $fishesCount . PHP_EOL;
-        //echo 'fishes: ' . implode(',', $currentFishes) . PHP_EOL;
-        //echo 'days to go: '. $daysToGo  . PHP_EOL;
-        //echo 'current day: ' . $day . ' fish: ' . $globalFishCounter . ' from ' . count($fishes) .  PHP_EOL;
         $fishesToUnset = [];
         for ($fishCounter = 0; $fishCounter < $fishesCount; $fishCounter++) {
-            //echo implode(', ', $currentFishes) . PHP_EOL;
             $currentFish = $currentFishes[$fishCounter];
             if (isset($cache[$currentFish][$daysToGo + 1])) {
-                //echo "for $currentFish with days {($daysToGo + 1)} to go add to total: " . $cache[$currentFish][$daysToGo + 1] . PHP_EOL;
-                //var_dump($cache);exit();
-                //$total += $cache[$currentFish][$daysToGo + 1];
                 $currentFishTotal += $cache[$currentFish][$daysToGo + 1];
                 $numberOfRemovedFishes++;
                 $fishesToUnset[] = $fishCounter;
@@ -52,23 +40,14 @@ foreach ($fishes as $fish) {
             unset($currentFishes[$fishToUnset]);
         }
         if (!isset($cache[$fish][$day]) && $numberOfRemovedFishes === 0) {
-            //echo "count current fishes " . count($currentFishes) . PHP_EOL;
-            //echo "currentfishtotal " . $currentFishTotal . PHP_EOL;
-            //var_dump($currentFishes);
-            //echo "fishes to unset " . count($fishesToUnset) . PHP_EOL;
-            //echo "number of removed fishes " . $numberOfRemovedFishes . PHP_EOL;
-            //echo 'zet in cache ' . $fish . ' ' . $day . ' ' . (count($currentFishes)) . PHP_EOL;
-            $cache[$fish][$day] = count($currentFishes) + $numberOfRemovedFishes;
-        } elseif (!isset($cache[$fish][$day]) && $daysToGo === 0) {
-            //echo 'zet in cache totaal ' . $fish . ' ' . $day . ' ' . ($currentFishTotal + count($currentFishes)) . PHP_EOL;
-            $cache[$fish][$day] = $currentFishTotal + count($currentFishes);
+            $cache[$fish][$day] = count($currentFishes);
         }
         $currentFishes = array_values($currentFishes);
-        //echo 'fishes end of day: ' . implode(',', $currentFishes) . PHP_EOL;
-        //echo 'in current total: ' . $currentFishTotal . PHP_EOL;
     }
-    //echo "add to total remaining number of fishes: " . $currentFishTotal . PHP_EOL . PHP_EOL;
     $total += $currentFishTotal + count($currentFishes);
 }
+echo $total . PHP_EOL;
 
-echo $total;
+$endTime = microtime(true);
+$executionTime = ($endTime - $startTime) * 1000;
+echo "Execution time: " . round($executionTime, 2) . " ms" . PHP_EOL;
