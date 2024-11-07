22b5d9d56cc315c7500aaa39cd9d181b9bf4c5ab
Maarten Vliegenthart <maarten@maximum.nl>
2024-11-07 23:28:02 +0100

    6b in other way

--- 6b.php
+++ 6b.php
@@ -2,11 +2,49 @@
 $startTime = microtime(true);
 $input = file('input6.txt');
 //echo $input[0] . "\n";
-$fishes = explode( ',', $input[0]);
+$fishes = explode(',', $input[0]);
 
 $total = 0;
 $totalDays = 256;
-$cache = [];
+$totals = [0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0];
+$nextStep = [0 => [6, 8], 1 => [0], 2 => [1], 3 => [2], 4 => [3], 5 => [4], 6 => [5], 7 => [6], 8 => [7]];
+for ($days = 2; $days <= $totalDays; $days *= 2) {
+    for ($fish = 0; $fish <= 8; $fish++) {
+        if ($days === $totalDays && $fish > max($fishes)) {
+            continue;
+        }
+        $newArray = [];
+        foreach ($nextStep[$fish] as $currentFish) {
+            if ($days === $totalDays) {
+                $totals[$fish] += count($nextStep[$currentFish]);
+            } else {
+                array_push($newArray, ...$nextStep[$currentFish]);
+            }
+        }
+        $newNextStepArray[$fish] = $newArray;
+    }
+    $nextStep = $newNextStepArray;
+}
+foreach ($fishes as $fish) {
+    $total += $totals[$fish];
+}
+echo $total . PHP_EOL;
+$endTime = microtime(true);
+$executionTime = ($endTime - $startTime) * 1000;
+echo "Execution time: " . round($executionTime, 2) . " ms" . PHP_EOL;
+exit();
+
+
+
+
+
+
+
+
+
+//alternative:
+
+
 foreach ($fishes as $fish) {
     $currentFishes = [$fish];
     $currentFishTotal = 0;
@@ -36,7 +74,7 @@ foreach ($fishes as $fish) {
                 $currentFishes[$fishCounter]--;
             }
         }
-        foreach($fishesToUnset as $fishToUnset) {
+        foreach ($fishesToUnset as $fishToUnset) {
             unset($currentFishes[$fishToUnset]);
         }
         if ($fishesAreRemoved) {
