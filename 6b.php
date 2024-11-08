8ece94b824b145528c9572f62e4072d8ac2a4e52
Maarten Vliegenthart <maarten@maximum.nl>
2024-11-08 09:11:47 +0100

    remove old alternative

--- 6b.php
+++ 6b.php
@@ -1,7 +1,6 @@
 <?php
 $startTime = microtime(true);
 $input = file('input6.txt');
-//echo $input[0] . "\n";
 $fishes = explode(',', $input[0]);
 
 $total = 0;
@@ -40,64 +39,6 @@ foreach ($fishes as $fish) {
     $total += $totals[$fish];
 }
 echo $total . PHP_EOL;
-$endTime = microtime(true);
-$executionTime = ($endTime - $startTime) * 1000;
-echo "Execution time: " . round($executionTime, 2) . " ms" . PHP_EOL;
-exit();
-
-
-
-
-
-
-
-
-
-//alternative:
-
-
-foreach ($fishes as $fish) {
-    $currentFishes = [$fish];
-    $currentFishTotal = 0;
-    $fishesAreRemoved = false;
-    for ($day = 1; $day <= $totalDays; $day++) {
-        $fishesCount = count($currentFishes);
-        $daysToGo = $totalDays - $day;
-        if ($fishesCount === 0) {
-            if (!isset($cache[$fish][$day])) {
-                $cache[$fish][$totalDays] = $currentFishTotal + count($currentFishes);
-            }
-            break;
-        }
-        $fishesToUnset = [];
-        for ($fishCounter = 0; $fishCounter < $fishesCount; $fishCounter++) {
-            $currentFish = $currentFishes[$fishCounter];
-            if (isset($cache[$currentFish][$daysToGo + 1])) {
-                $currentFishTotal += $cache[$currentFish][$daysToGo + 1];
-                $fishesAreRemoved = true;
-                $fishesToUnset[] = $fishCounter;
-                continue;
-            }
-            if ($currentFish === 0) {
-                $currentFishes[] = 8;
-                $currentFishes[$fishCounter] = 6;
-            } else {
-                $currentFishes[$fishCounter]--;
-            }
-        }
-        foreach ($fishesToUnset as $fishToUnset) {
-            unset($currentFishes[$fishToUnset]);
-        }
-        if ($fishesAreRemoved) {
-            $currentFishes = array_values($currentFishes);
-        } elseif (!isset($cache[$fish][$day])) {
-            $cache[$fish][$day] = count($currentFishes);
-        }
-    }
-    $total += $currentFishTotal + count($currentFishes);
-}
-echo $total . PHP_EOL;
-
 $endTime = microtime(true);
 $executionTime = ($endTime - $startTime) * 1000;
 echo "Execution time: " . round($executionTime, 2) . " ms" . PHP_EOL;
