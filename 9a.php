da98935ee2f1a00852c3da3ff2de0d915778e1a7
Maarten Vliegenthart <maarten@maximum.nl>
2024-11-16 13:02:03 +0100

    9a

--- 9a.php
+++ 9a.php
@@ -7,8 +7,25 @@ foreach ($input as $index => $line) {
     $heights[$index] = str_split(str_replace("\n",'',$line));
 }
 $nrOfLines = count($heights);
-for ($row = 0; $row <= $nrOfLines; $row++) {
-    for ($col = 0; $col < count($heights[$i]); $col++) {
-        if ($heights[$row - 1]?[$col] && $heights[$row - 1]?[$col] )
+$total = 0;
+for ($row = 0; $row < $nrOfLines; $row++) {
+    for ($col = 0; $col < count($heights[$row]); $col++) {
+        if (isset($heights[$row - 1][$col]) && ($heights[$row - 1][$col] < $heights[$row][$col])) {
+            continue;
+        }
+        if (isset($heights[$row][$col - 1]) && ($heights[$row][$col - 1] < $heights[$row][$col])) {
+            continue;
+        }
+        if (isset($heights[$row][$col + 1]) && ($heights[$row][$col + 1] < $heights[$row][$col])) {
+            continue;
+        }
+        if (isset($heights[$row + 1][$col]) && ($heights[$row + 1][$col] < $heights[$row][$col])) {
+            continue;
+        }
+        $total += 1 + $heights[$row][$col];
     }
 }
+echo $total . PHP_EOL;
+$endTime = microtime(true);
+$executionTime = ($endTime - $startTime) * 1000;
+echo "Execution time: " . round($executionTime, 2) . " ms" . PHP_EOL;
