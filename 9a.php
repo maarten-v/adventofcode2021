a90f086d9a9d6fa937827eb9ced9232c4e7a89c6
Maarten <maarten@vliegenthart.org>
2024-11-17 21:38:37 +0100

    9a

--- 9a.php
+++ 9a.php
@@ -2,37 +2,29 @@
 $startTime = microtime(true);
 $input = file('input9.txt');
 
-$heights = [];
-foreach ($input as $index => $line) {
-    $heights[$index] = str_split(str_replace("\n",'',$line));
-}
+$heights = array_map(static function ($line) {
+    return str_split(str_replace("\n", '', $line));
+}, $input);
 $nrOfLines = count($heights);
 $total = 0;
+$lineLength = count($heights[0]);
 for ($row = 0; $row < $nrOfLines; $row++) {
-    for ($col = 0; $col < count($heights[$row]); $col++) {
+    for ($col = 0; $col < $lineLength; $col++) {
         $higherFound = false;
         $lowerFound = false;
-        checkHeight($heights[$row][$col], $heights[$row - 1][$col], $higherFound, $lowerFound);
-        checkHeight($heights[$row][$col], $heights[$row - 1][$col], $higherFound, $lowerFound);
-        checkHeight($heights[$row][$col], $heights[$row - 1][$col], $higherFound, $lowerFound);
-        checkHeight($heights[$row][$col], $heights[$row - 1][$col], $higherFound, $lowerFound);
-
-        if (isset($heights[$row - 1][$col])) {
-            if ($heights[$row - 1][$col] > $heights[$row][$col]) {
-                $higherFound = true;
-            } elseif ($heights[$row - 1][$col] < $heights[$row][$col]) {
-                continue;
-            }
-        }
-        if (!$higherFound) {
+        checkHeight($heights[$row][$col], $heights[$row - 1][$col] ?? null, $higherFound, $lowerFound);
+        checkHeight($heights[$row][$col], $heights[$row][$col - 1] ?? null, $higherFound, $lowerFound);
+        checkHeight($heights[$row][$col], $heights[$row][$col + 1] ?? null, $higherFound, $lowerFound);
+        checkHeight($heights[$row][$col], $heights[$row + 1][$col] ?? null, $higherFound, $lowerFound);
+        if (!$higherFound || $lowerFound) {
             continue;
         }
-        echo $row . '-' . $col. PHP_EOL;
         $total += 1 + $heights[$row][$col];
     }
 }
 
-function checkHeight($selfHeight, $surroundingHeight, &$higherFound, &$lowerFound) {
+function checkHeight($selfHeight, $surroundingHeight, &$higherFound, &$lowerFound): void
+{
     if ($lowerFound) {
         return;
     }
