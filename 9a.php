4daf0303ed869d84069014a4c5e98a3ad098bb45
Maarten Vliegenthart <maarten@maximum.nl>
2024-11-17 16:29:13 +0100

    9a wip

--- 9a.php
+++ 9a.php
@@ -1,6 +1,6 @@
 <?php
 $startTime = microtime(true);
-$input = file('testinput9.txt');
+$input = file('input9.txt');
 
 $heights = [];
 foreach ($input as $index => $line) {
@@ -10,21 +10,42 @@ $nrOfLines = count($heights);
 $total = 0;
 for ($row = 0; $row < $nrOfLines; $row++) {
     for ($col = 0; $col < count($heights[$row]); $col++) {
-        if (isset($heights[$row - 1][$col]) && ($heights[$row - 1][$col] < $heights[$row][$col])) {
+        $higherFound = false;
+        $lowerFound = false;
+        checkHeight($heights[$row][$col], $heights[$row - 1][$col], $higherFound, $lowerFound);
+        checkHeight($heights[$row][$col], $heights[$row - 1][$col], $higherFound, $lowerFound);
+        checkHeight($heights[$row][$col], $heights[$row - 1][$col], $higherFound, $lowerFound);
+        checkHeight($heights[$row][$col], $heights[$row - 1][$col], $higherFound, $lowerFound);
+
+        if (isset($heights[$row - 1][$col])) {
+            if ($heights[$row - 1][$col] > $heights[$row][$col]) {
+                $higherFound = true;
+            } elseif ($heights[$row - 1][$col] < $heights[$row][$col]) {
+                continue;
+            }
+        }
+        if (!$higherFound) {
             continue;
         }
-        if (isset($heights[$row][$col - 1]) && ($heights[$row][$col - 1] < $heights[$row][$col])) {
-            continue;
-        }
-        if (isset($heights[$row][$col + 1]) && ($heights[$row][$col + 1] < $heights[$row][$col])) {
-            continue;
-        }
-        if (isset($heights[$row + 1][$col]) && ($heights[$row + 1][$col] < $heights[$row][$col])) {
-            continue;
-        }
+        echo $row . '-' . $col. PHP_EOL;
         $total += 1 + $heights[$row][$col];
     }
 }
+
+function checkHeight($selfHeight, $surroundingHeight, &$higherFound, &$lowerFound) {
+    if ($lowerFound) {
+        return;
+    }
+    if (isset($surroundingHeight)) {
+        if ($surroundingHeight > $selfHeight) {
+            $higherFound = true;
+        }
+        if ($surroundingHeight < $selfHeight) {
+            $lowerFound = true;
+        }
+    }
+}
+
 echo $total . PHP_EOL;
 $endTime = microtime(true);
 $executionTime = ($endTime - $startTime) * 1000;
